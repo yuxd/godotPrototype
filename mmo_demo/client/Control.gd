29146839fda81a037ab485bf97d9235d0ab4f886
liweimin <2410250284@qq.com>
2022-01-06 10:05:44 +0800

    收发聊天消息

---
+++ mmo_demo/client/Control.gd
@@ -0,0 +1,21 @@
+extends Control
+
+onready var text_edit = $TextEdit
+onready var lable_chat = $Label
+const MyProto  = preload("res://pb/msg.proto.gd")
+
+func _ready():
+	lable_chat.text = ""
+	Client.connect("broadcast_world_chat",self,"broadcast_world_chat")
+
+func _on_Button_pressed():
+	var talk_content = text_edit.text
+	var data = MyProto.Talk.new()
+	data.set_Content(talk_content)
+	var msg_id = 2
+	Client.send_message(msg_id, data)
+
+func broadcast_world_chat(player_id,content):
+	print("content: ", content)
+	var t = lable_chat.text
+	lable_chat.text = t + "player ID: " + player_id + " connent : " + content + "/n"

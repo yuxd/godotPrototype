b6a253451ae2a3922709d165f84b1a38819d4cbc
liweimin <2410250284@qq.com>
2022-01-13 20:31:24 +0800

    开始界面和设置弹窗

---
+++ applePicker/Scenes/Start.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_btn_Exit_pressed():
+	# "退出游戏"
+	get_tree().quit()
+
+
+
+func _on_btn_Setting_pressed():
+	pass # Replace with function body.
+
+
+func _on_btn_Start_pressed():
+	pass # Replace with function body.

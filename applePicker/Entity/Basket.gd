89a8da4c1aeea09c3579cc44627a299f246b85f8
liweimin <2410250284@qq.com>
2022-01-13 13:03:52 +0800

    主角移动

---
+++ applePicker/Entity/Basket.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+export (int) var speed = 200
+export (int) var x_min = 33
+export (int) var x_max = 991
+
+var velocity = Vector2()
+
+func _ready():
+	pass
+
+func get_input():
+	velocity = Vector2()
+	if Input.is_action_pressed('ui_right'):
+		velocity.x += 1
+	if Input.is_action_pressed('ui_left'):
+		velocity.x -= 1
+	# if Input.is_action_pressed('move_down'):
+	# 	velocity.y += 1
+	# if Input.is_action_pressed('move_up'):
+	# 	velocity.y -= 1
+	velocity = velocity.normalized() * speed
+
+func _physics_process(delta):
+	get_input()
+	velocity = move_and_slide(velocity)
+	if position.x >= x_max :
+		position.x = x_max
+	elif position.x <= x_min:
+		position.x = x_min

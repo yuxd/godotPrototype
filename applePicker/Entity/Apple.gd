7dceb56503bf2d56e05fd4e8f2d4713ce9fe49c3
liweimin <2410250284@qq.com>
2022-01-14 10:03:16 +0800

    苹果和果篮的碰撞检测

--- applePicker/Entity/Apple.gd
+++ applePicker/Entity/Apple.gd
@@ -10,5 +10,8 @@ func _process(delta):
 	if position.y >= 550:
 		self.queue_free()
 
-func _on_Area2D_area_entered():
-	pass
+
+
+func _on_Area2D_body_entered(body:Node):
+	print("!",body.name)
+	pass # Replace with function body.

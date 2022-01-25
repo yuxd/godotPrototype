b474fa318209f496e28321534a7d75225d33fb41
liweimin <2410250284@qq.com>
2022-01-25 15:29:57 +0800

    状态机和行为树

---
+++ survival/behavior_tree/composite/composite_Parallel.gd
@@ -0,0 +1,35 @@
+extends Task
+
+# Run all child Tasks together in SEQUENCE or SELECTOR policy mode
+
+class_name Parallel
+
+enum { SEQUENCE, SELECTOR }
+
+export(bool) var policy = SEQUENCE
+
+var num_results = 0
+
+func run():
+	for child in get_children():
+		child.run()
+	running()
+
+func child_success():
+	num_results += 1
+	if policy == bool(SEQUENCE):
+		if num_results >= get_child_count():
+			num_results = 0
+			success()
+	else:
+		success()
+
+func child_fail():
+	num_results += 1
+	if policy == bool(SELECTOR):
+		if num_results >= get_child_count():
+			num_results = 0
+			fail()
+	else:
+		fail()
+

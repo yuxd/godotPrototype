235d9ad13584e605b169e1a0269f48c1c5dfe07e
liweimin <2410250284@qq.com>
2022-01-26 18:43:42 +0800

    场景管理器单例

--- game_demo_gd/spaceShooter/addons/QFramework/q_instance.gd
+++ game_demo_gd/spaceShooter/addons/QFramework/q_instance.gd
@@ -2,14 +2,11 @@ extends Node
 
 const datatable_manager_class = preload("res://addons/QFramework/datatable/csv_datatable_manager.gd")
 const procedure_manager_class = preload("res://addons/QFramework/procedure/manager_procedure.gd")
-
+const scene_manager_class = preload("res://addons/QFramework/manager_scene.gd")
 var _datatable_manager : DatatableManager
 var _procedure_manager : ProcedureManager
+var _scene_manager : SceneManager
 var config = ConfigFile.new()
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
 
 func get_datatable_manager() -> DatatableManager:
 	if !_datatable_manager:
@@ -22,3 +19,9 @@ func get_procedure_manager() -> ProcedureManager:
 		_procedure_manager = procedure_manager_class.new()
 		self.add_child(_procedure_manager)
 	return _procedure_manager
+
+func get_scene_manager() -> SceneManager:
+	if !_scene_manager:
+		_scene_manager = scene_manager_class.new()
+		self.add_child(_scene_manager)
+	return _scene_manager

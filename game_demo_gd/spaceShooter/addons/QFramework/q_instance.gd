3f2665872d538e8813e42b743e7f78ad076152d4
liweimin <2410250284@qq.com>
2022-01-26 13:16:46 +0800

    将 gameinstance 改为 q_instance 作为框架的一部分，开发者也可以自定义自己的instance，这里只是提供一种思路

--- game_demo_gd/spaceShooter/addons/QFramework/q_instance.gd
+++ game_demo_gd/spaceShooter/addons/QFramework/q_instance.gd
@@ -1,17 +1,15 @@
 extends Node
 
+const datatable_manager_class = preload("res://addons/QFramework/managers/datatable/csv_datatable_manager.gd")
+
 var _datatable_manager : DatatableManager
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	_datatable_manager = CSVDatatableManager.new()
-	self.add_child(_datatable_manager)
-	_datatable_manager.set_datatable_path("res://datatables/")
-	var d = _datatable_manager.get_datatable("Aircraft")
-	print(d)
+	pass
 
 func get_datatable_manager() -> DatatableManager:
 	if !_datatable_manager:
-		_datatable_manager = CSVDatatableManager.new()
+		_datatable_manager = datatable_manager_class.new()
 		self.add_child(_datatable_manager)
 	return _datatable_manager

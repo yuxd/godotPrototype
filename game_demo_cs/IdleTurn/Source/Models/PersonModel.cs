75a2c55863ab939a519689fbf356790d65cfbba5
liweimin <weimin.li@gaea.com>
2022-07-22 17:48:10 +0800

    IdleTurn

---
+++ game_demo_cs/IdleTurn/Source/Models/PersonModel.cs
@@ -0,0 +1,10 @@
+namespace GameSource.Models
+{
+    public class PersonModel
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+        public int AGE { get; set; }
+        public string Sex { get; set; }
+    }
+}

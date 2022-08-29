0e860149610d3a0368e0a8a1c704148763bf734c
liweimin <weimin.li@gaea.com>
2022-08-29 13:17:59 +0800

    刷新器实现

--- game_demo_gd_4/mmo_demo/client/source/components/c_refreshable.gd
+++ game_demo_gd_4/mmo_demo/client/source/components/c_refreshable.gd
@@ -11,18 +11,21 @@ var subordinates : Array = []
 
 @export var subordinate_random_pool : Array = []
 @export var subordinate_random_widget : PackedInt32Array = []
+@export var random_range := 0 
 
 var _total_widget : int = 0
 
 func _ready():
+	time_left = refresh_inverval
 	for w in subordinate_random_widget:
 		_total_widget += w
 
 func can_refresh() -> bool:
-	if subordinate_count < get_subordinate_count() or subordinate_count == -1 :
+	if subordinate_count < get_subordinate_count() or subordinate_count == -1 or subordinates.is_empty() :
 		return true
 	return false
-	
+
+
 func get_subordinate_count() -> int:
 	var count : int = 0
 	for s in subordinates:
@@ -32,6 +35,7 @@ func get_subordinate_count() -> int:
 			count += 1
 	return count
 
+
 func get_random_subordinate() -> PackedScene:
 	var r = randi_range(0, _total_widget)
 	var widget := 0
@@ -40,4 +44,11 @@ func get_random_subordinate() -> PackedScene:
 		if r <= w:
 			return subordinate_random_pool[i]
 	return null
-	
+
+
+func get_refresh_position() -> Vector2:
+	if random_range == 0:
+		return Vector2.ZERO
+	var x = randi_range(0, random_range)
+	var y = random_range^2 - x^2
+	return Vector2(x, y)
--- game_demo_gd_4/mmo_demo/client/source/systems/s_refresh.gd
+++ game_demo_gd_4/mmo_demo/client/source/systems/s_refresh.gd
@@ -3,24 +3,26 @@ class_name RefreshSystem
 
 func _init():
 	system_name = "S_Refresh"
-	requirements = ["C_Refreshable"]
+	requirements = ["C_Refreshable", "C_Prescene"]
 
 
 func _system_process(_entities: Array, delta: float) -> void:
 	for e in _entities:
-		var refreshable : RefreshableComponent = e.get_component("C_Refreshable")
+		var refreshable : RefreshableComponent = e.get_component("C_Refreshable")		
 		if refreshable.time_left <= 0:
 			# 刷新逻辑
-			var new_c = active_refresh(refreshable)
+			active_refresh(e)
 			refreshable.time_left == refreshable.refresh_inverval
 		else :
 			# 减时间逻辑
 			refreshable.time_left = refreshable.time_left - delta
 
 
-func active_refresh(ref : RefreshableComponent) -> Entity:
+func active_refresh(ref_entity : Entity) -> void:
+	var ref : RefreshableComponent = ref_entity.get_component("C_Refreshable")
+	var prescene : PresceneComponent = ref_entity.get_component("C_Prescene")
 	if ref.can_refresh():
 		var sub = ref.get_random_subordinate().instantiate()
 		ref.subordinates.append(sub)
-		return sub
-	return null
+		sub.position = ref_entity.position + ref.get_refresh_position()
+		prescene.owner_scene.add_child(sub)
